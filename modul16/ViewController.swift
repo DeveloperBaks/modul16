2f6598087a16a385db688f5fc1787c67e8dfe250
Andrey <andreybakastov@mail.ru>
2024-05-08 19:56:27 +0300

    Initial Commit

---
+++ modul16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  modul16
+//
+//  Created by andreybakastov@mail.ru on 08.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
